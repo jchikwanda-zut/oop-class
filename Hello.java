63d6666614506af5c35771edd9a447f0a5aa233a
Jacob Chikwanda <jchikwanda@zut.edu.zm>
2026-08-20 15:28:32 +0200

    Update Hello.java
    Removed function to print cli args

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World" + " " +  args.length);
-        for (String string : args) {
-            System.out.println(string);
-        }
     }
 }
